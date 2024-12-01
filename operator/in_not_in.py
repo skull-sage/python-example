3e3c107a0215d06d472910fffdc20d3feccf3fa7
Muhammad Rashed Alam <60359@palonet.org>
2024-12-01 19:52:25 +0600

    Overloading in progress

--- operator/in_not_in.py
+++ operator/in_not_in.py
@@ -1,7 +1,15 @@
+"""
+    in and not in 
+    is generally for membership checking
+    be cautious when membership checking of empty string: it will always return true 
 
-def checkExist(*args):
-    print(f"{args[0]} in {args[1]}? {args[0] in args[1]}")
+"""
 
-checkExist((1, 2), (1, 2, 3, 4))
-checkExist({1, 2}, {1, 4, 3, 2})
-checkExist((1, 2), [1, 4, 3, 2])
+print( 5 in {1, 2, 5}) # True
+print( 5 in (1, 2, 5)) # True
+print( 5 in [1, 2, 5]) # True
+print("" in "abcd") # always True
+
+print( 5 in {1, 2, 6}) # False
+
+
